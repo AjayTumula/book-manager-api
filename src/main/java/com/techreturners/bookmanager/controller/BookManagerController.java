d5556baba1c69b20f0582b92cec3f1e5c89edb65
AjayTumula <ajaytumula72@gmail.com>
2023-03-12 13:39:37 +0000

    Made changes in the Controller class

--- src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
+++ src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
@@ -1,5 +1,7 @@
 package com.techreturners.bookmanager.controller;
 
+import com.techreturners.bookmanager.exception.BookAlreadyExistsException;
+import com.techreturners.bookmanager.exception.BookNotFoundException;
 import com.techreturners.bookmanager.model.Book;
 import com.techreturners.bookmanager.service.BookManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +27,19 @@ public class BookManagerController {
 
     @GetMapping({"/{bookId}"})
     public ResponseEntity<Book> getBookById(@PathVariable Long bookId) {
-        return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        Book book = bookManagerService.getBookById(bookId);
+        if (book == null) {
+            throw new BookNotFoundException("The book is not present with this ID");
+        }
+        return new ResponseEntity<>(book, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book bookExisting = bookManagerService.getBookById(book.getId());
+        if (bookExisting != null) {
+            throw new BookAlreadyExistsException("There is already a book with the same Id, please try with some other ID");
+        }
         Book newBook = bookManagerService.insertBook(book);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
@@ -43,4 +53,17 @@ public class BookManagerController {
         return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
     }
 
+    @DeleteMapping({"/{bookId}"})
+    public ResponseEntity<Book> deleteBookById(@PathVariable("bookId") Long bookId) throws Exception {
+        Book book = bookManagerService.getBookById(bookId);
+        if (book == null) {
+            throw new BookNotFoundException("The requested book is not deleted, please try to delete existing book");
+        }
+        bookManagerService.deleteBookById(bookId);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
 }
+
+
+
